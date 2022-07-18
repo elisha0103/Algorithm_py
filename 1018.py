10ed6e7156730c9eb4363d3e0a398ecc97af571b
elisha0103 <41459466+elisha0103@users.noreply.github.com>
2022-07-18 21:47:37 +0900

    Update 1018.py

--- 1018.py
+++ 1018.py
@@ -1,26 +1,31 @@
-N, M = map(int, input().split())
-board = list()
-for i in range(N):
-    board.append(input())
-repair = list()
+n, m = map(int,input().split())
+board = []
+for i in range(n):
+    board.append(list(input()))
+data = []
+box_w = 0
+box_b = 0
 
-for i in range(N-7):
-    for j in range(M-7):
-        first_W = 0
-        first_B = 0
-        for k in range(i,i+8):
-            for l in range(j,j + 8):
-                if (k + l) % 2 == 0:
-                    if board[k][l] != 'W':
-                        first_W = first_W+1
-                    if board[k][l] != 'B':
-                        first_B = first_B + 1
+for i in range(n-7):
+    for j in range(m-7):
+        box_w = 0  #w로 시작했을 때 칠해야 하는 칸 개수
+        box_b = 0  #b로 시작했을 때 칠해야 하는 칸 개수
+        for k in range(i, i+8):
+            for l in range(j, j+8):
+
+                if (k+l) % 2 == 0:
+                    if board[k][l] != 'W': #만약 짝수 칸이 W어야 하는 경우, 근데 짝수 칸에 B가 있는 경우
+                        box_w += 1 #box_w로 칠한 횟수 증가
+                    else:         #만약 짝수 칸이 B어야 하는 경우, 근데 짝수 칸에 W가 있는 경우
+                        box_b += 1
                 else:
-                    if board[k][l] != 'B':
-                        first_W = first_W+1
-                    if board[k][l] != 'W':
-                        first_B = first_B + 1
-        repair.append(first_W)
-        repair.append(first_B)
+                    if board[k][l] != 'B': #만약 홀수 칸이 B어야 하는 경우, 근데 홀수 칸에 W가 있는 경우
+                        box_w += 1  #box_w로 칠한 횟수 증가
+                    else:           #만약 홀수 칸이 W어야 하는 경우, 근데 홀수 칸에 B가 있는 경우
+                        box_b += 1
+        data.append(box_w)   #b로 시작했는데 배열 틀려서 칠한 칸 개수
+        data.append(box_b)   #w로 시작했는데 배열 틀려서 칠한 칸 개수
 
-print(min(repair))
+
+
+print(min(data))
